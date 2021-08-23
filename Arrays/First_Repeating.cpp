9b41952c7c6a8abb68238d95763236b944896feb
AnuragUnnikannan <anuragunnikannan7@gmail.com>
2021-08-24 03:00:18 +0530

    Print the 1st occurrence of the element repeated

---
+++ Arrays/First_Repeating.cpp
@@ -0,0 +1,55 @@
+/*
+    Print the first occurrence of the element that is repeated.
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number of elements: " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for(int i = 0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    const int N = 100000;
+    int idx[N];
+    int min_index = INT_MAX;
+    for(int i = 0;i<N;i++)
+    {
+        idx[i] = -1;
+    }
+    for(int i = 0;i<n;i++)
+    {
+        //for repeating element
+        if(idx[arr[i]] != -1)
+        {
+            min_index = min(min_index, idx[arr[i]]);
+        }
+        else
+        {
+            //storing index of each element
+            idx[arr[i]] = i;
+        }
+    }
+    for(int i = 0;i<N;i++)
+    {
+        if(idx[i]!=-1)
+        {
+            cout << idx[i] << " ";
+        }
+    }
+    //if no repetition
+    if(min_index == INT_MAX)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << min_index + 1 << endl;
+    }
+    return 0;
+}
