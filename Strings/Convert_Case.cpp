ad460c2b70d010aa7af7ee3b2cb4a127175260c9
AnuragUnnikannan <anuragunnikannan7@gmail.com>
2021-08-24 03:13:25 +0530

    Changing case of a string using ASCII value

---
+++ Strings/Convert_Case.cpp
@@ -0,0 +1,36 @@
+/*
+    Changing case of a string using ASCII value
+*/
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter a string in lower case:" << endl;
+    getline(cin, str);
+    cout << endl;
+    //for lower to upper
+    for(int i = 0;i < str.length();i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            //by subtracting 32, the string gets converted to uppercase
+            str[i] -= 32;
+        }
+    }
+    cout << str << endl;
+
+    //for upper to lower
+    for(int i = 0;i < str.length();i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            //by adding 332, the string gets converted to lowercase
+            str[i] += 32;
+        }
+    }
+    cout << str << endl;
+    return 0;
+}
